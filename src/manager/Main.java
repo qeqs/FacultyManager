d34a877413ceb66efad43238fcee2e540b2f9b54
qeqs <vadimlygin@yandex.ru>
2016-11-29 22:32:05 +0300

    add maven dependencies

--- src/manager/Main.java
+++ src/manager/Main.java
@@ -10,12 +10,13 @@ public class Main {
     }
 
 
-
     private static final SessionFactory sessionFactory;
+
     static {
         try {
             sessionFactory = new Configuration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
+        }
+        catch (Throwable ex) {
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
